16bca2b7285bb23562b64e825476cd3c4204952f
siggy <john.sigler@gmail.com>
2014-10-14 11:17:23 -0500

    Tweak the example further

--- training/SimpleDesign/src/main/java/simpledesign/smells/calculator/Calculator.java
+++ training/SimpleDesign/src/main/java/simpledesign/smells/calculator/Calculator.java
@@ -2,16 +2,15 @@ package simpledesign.smells.calculator;
 
 public interface Calculator {
 
-    long add(long operand1, long operand2);
+   long add(long operand1, long operand2);
 
-    long subtract(long operand1, long operand2);
+   long subtract(long operand1, long operand2);
 
-    long multiply(long operand1, long operand2);
+   long multiply(long operand1, long operand2);
 
-    long divide(long operand1, long operand2);
+   long divide(long operand1, long operand2);
 
+   double sin(double value);
 
-    double sin(double value);
-
-    double cos(double value);
+   double cos(double value);
 }
